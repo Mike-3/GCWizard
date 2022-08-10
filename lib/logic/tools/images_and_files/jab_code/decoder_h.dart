ad9cd57f0f88aa31106932ee4354da075c150742
Mike-3 <Mike.Binnemann@web.de>
2022-08-10 14:40:43 +0200

    first h files

---
+++ lib/logic/tools/images_and_files/jab_code/decoder_h.dart
@@ -0,0 +1,77 @@
+import 'dart:typed_data';
+
+import 'jabcode_h.dart';
+
+/**
+ * libjabcode - JABCode Encoding/Decoding Library
+ *
+ * Copyright 2016 by Fraunhofer SIT. All rights reserved.
+ * See LICENSE file for full terms of use and distribution.
+ *
+ * Contact: Huajian Liu <liu@sit.fraunhofer.de>
+ *			Waldemar Berchtold <waldemar.berchtold@sit.fraunhofer.de>
+ *
+ * @file decoder.h
+ * @brief Decoder header
+ */
+
+//#ifndef JABCODE_DECODER_H
+//#define JABCODE_DECODER_H
+
+const DECODE_METADATA_FAILED = -1;
+const FATAL_ERROR = -2;	//e.g. out of memory
+
+const MASTER_METADATA_X =	6;
+const MASTER_METADATA_Y	= 1;
+
+const MASTER_METADATA_PART1_LENGTH = 6;			//master metadata part 1 encoded length
+const MASTER_METADATA_PART2_LENGTH = 38;			//master metadata part 2 encoded length
+const MASTER_METADATA_PART1_MODULE_NUMBER = 4;	//the number of modules used to encode master metadata part 1
+
+/**
+ * @brief The positions of the first 32 color palette modules in slave symbol
+*/
+final slave_palette_position = List<jab_vector2d>.from(
+		[	{4, 5}, {4, 6}, {4, 7}, {4, 8}, {4, 9}, {4, 10}, {4, 11}, {4, 12},
+			{5, 12}, {5, 11}, {5, 10}, {5, 9}, {5, 8}, {5, 7}, {5, 6}, {5, 5},
+			{6, 5}, {6, 6}, {6, 7}, {6, 8}, {6, 9}, {6, 10}, {6, 11}, {6, 12},
+			{7, 12}, {7, 11}, {7, 10}, {7, 9}, {7, 8}, {7, 7}, {7, 6}, {7, 5}
+		]);
+
+/**
+ * @brief Decoding tables
+*/
+final  jab_decoding_table_upper   = Int8List.fromList([32, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90]);
+final jab_decoding_table_lower   = Int8List.fromList([32, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]);
+final jab_decoding_table_numeric = Int8List.fromList([32, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 44, 46]);
+final jab_decoding_table_punct   = Int8List.fromList([33, 34, 36, 37, 38, 39, 40, 41, 44, 45, 46, 47, 58, 59, 63, 64]);
+final jab_decoding_table_mixed   = Int8List.fromList([35, 42, 43, 60, 61, 62, 91, 92, 93, 94, 95, 96, 123, 124, 125, 126, 9, 10, 13, 0, 0, 0, 0, 164, 167, 196, 214, 220, 223, 228, 246, 252]);
+final jab_decoding_table_alphanumeric = Int8List.fromList([32, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85,
+															 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122]);
+
+/**
+ * @brief Encoding mode
+*/
+class jab_encode_mode {
+	static var None = -1;
+	static var Upper = 0;
+	static var Lower = 1;
+	static var Numeric = 2;
+	static var Punct = 3;
+	static var Mixed = 4;
+	static var Alphanumeric = 5;
+	static var Byte = 6;
+	static var ECI = 7;
+	static var FNC1 = 8;
+}
+
+// extern jab_int32 decodeMaster(jab_bitmap* matrix, jab_decoded_symbol* symbol);
+// extern jab_int32 decodeSlave(jab_bitmap* matrix, jab_decoded_symbol* symbol);
+// extern jab_data* decodeData(jab_data* bits);
+// extern void deinterleaveData(jab_data* data);
+// extern void getNextMetadataModuleInMaster(jab_int32 matrix_height, jab_int32 matrix_width, jab_int32 next_module_count, jab_int32* x, jab_int32* y);
+// extern void demaskSymbol(jab_data* data, jab_byte* data_map, jab_vector2d symbol_size, jab_int32 mask_type, jab_int32 color_number);
+// extern jab_int32 readColorPaletteInMaster(jab_bitmap* matrix, jab_decoded_symbol* symbol, jab_byte* data_map, jab_int32* module_count, jab_int32* x, jab_int32* y);
+// extern jab_int32 readColorPaletteInSlave(jab_bitmap* matrix, jab_decoded_symbol* symbol, jab_byte* data_map);
+//
+// #endif
---
+++ lib/logic/tools/images_and_files/jab_code/jabcode_h.dart
@@ -0,0 +1,180 @@
+import 'dart:core';
+import 'dart:ffi';
+import 'dart:typed_data';
+
+/**
+ * libjabcode - JABCode Encoding/Decoding Library
+ *
+ * Copyright 2016 by Fraunhofer SIT. All rights reserved.
+ * See LICENSE file for full terms of use and distribution.
+ *
+ * Contact: Huajian Liu <liu@sit.fraunhofer.de>
+ *			Waldemar Berchtold <waldemar.berchtold@sit.fraunhofer.de>
+ *
+ * @file jabcode.h
+ * @brief Main libjabcode header
+ */
+
+// #ifndef JABCODE_H
+// const JABCODE_H
+
+const VERSION ="2.0.0";
+//const BUILD_DATE = __DATE__
+
+const MAX_SYMBOL_NUMBER       = 61;
+const MAX_COLOR_NUMBER        = 256;
+const MAX_SIZE_ENCODING_MODE  = 256;
+const JAB_ENCODING_MODES      = 6;
+const ENC_MAX                 = 1000000;
+const NUMBER_OF_MASK_PATTERNS	= 8;
+
+const DEFAULT_SYMBOL_NUMBER 		=	1;
+const DEFAULT_MODULE_SIZE				= 12;
+const DEFAULT_COLOR_NUMBER 			= 8;
+const DEFAULT_MODULE_COLOR_MODE =	2;
+const DEFAULT_ECC_LEVEL				  = 3;
+const DEFAULT_MASKING_REFERENCE = 7;
+
+
+const DISTANCE_TO_BORDER      = 4;
+const MAX_ALIGNMENT_NUMBER    = 9;
+const COLOR_PALETTE_NUMBER	= 4;
+
+const BITMAP_BITS_PER_PIXEL	= 32;
+const BITMAP_BITS_PER_CHANNEL	= 8;
+const BITMAP_CHANNEL_COUNT	= 4;
+
+const	JAB_SUCCESS		= 1;
+const	JAB_FAILURE		= 0;
+
+const NORMAL_DECODE		= 0;
+const COMPATIBLE_DECODE	= 1;
+
+// const VERSION2SIZE(x)		(x * 4 + 17)
+// const SIZE2VERSION(x)		((x - 17) / 4)
+// const MAX(a,b) 			({__typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a > _b ? _a : _b;})
+// const MIN(a,b) 			({__typeof__ (a) _a = (a); __typeof__ (b) _b = (b); _a < _b ? _a : _b;})
+
+JAB_REPORT_ERROR(String x)	{ print("JABCode Error: "); print (x); print("\n"); }
+JAB_REPORT_INFO(String x)	{ print("JABCode Info: "); print (x); print("\n"); }
+
+//typedef byte 		jab_byte;
+//typedef char 				jab_char;
+//typedef unsigned char 		jab_boolean;
+//typedef int 				jab_int32;
+//typedef unsigned int 		jab_uint32;
+//typedef short 				jab_int16;
+//typedef unsigned short 		jab_uint16;
+//typedef long long 			jab_int64;
+//typedef unsigned long long	jab_uint64;
+//typedef float				jab_float;
+//typedef double              jab_double;
+
+/**
+ * @brief 2-dimensional integer vector
+*/
+class jab_vector2d {
+	int	x;
+	int	y;
+}
+
+/**
+ * @brief 2-dimensional float vector
+*/
+class jab_point {
+	double	x;
+double	y;
+}
+
+/**
+ * @brief Data structure
+*/
+class jab_data {
+	int	length;
+	String	data;
+}
+
+/**
+ * @brief Code bitmap
+*/
+class jab_bitmap {
+   int	width;
+   int	height;
+   int	bits_per_pixel;
+   int	bits_per_channel;
+   int	channel_count;
+   Int8List		pixel;
+}
+
+/**
+ * @brief Symbol parameters
+*/
+class jab_symbol {
+	int		index;
+	jab_vector2d	side_size;
+	int		host;
+	var		slaves = List<int>.filled(4,0); //[4]
+	var 		wcwr =  List<int>.filled(2,0); //[2]
+	jab_data		data;
+	Int8List		data_map;
+	jab_data		metadata;
+	Int8List		matrix;
+}
+
+/**
+ * @brief Encode parameters
+*/
+class jab_encode {
+	int		color_number;
+	int		symbol_number;
+	int		module_size;
+	int		master_symbol_width;
+	int		master_symbol_height;
+	Int8List		palette;				///< Palette holding used module colors in format RGB
+	jab_vector2d	symbol_versions;
+	Int8List 		symbol_ecc_levels;
+	Int32List		symbol_positions;
+	jab_symbol		symbols;				///< Pointer to internal representation of JAB Code symbols
+	jab_bitmap		bitmap;
+}
+
+/**
+ * @brief Decoded metadata
+*/
+class jab_metadata {
+	bool default_mode;
+	Int8 Nc;
+	Int8 mask_type;
+	Int8 docked_position;
+	jab_vector2d side_version;
+	jab_vector2d ecl;
+}
+
+/**
+ * @brief Decoded symbol
+*/
+class jab_decoded_symbol {
+	int index;
+	int host_index;
+	int host_position;
+	jab_vector2d side_size;
+	double module_size;
+	var pattern_positions = List<jab_point>.filled(4, null); //4
+	jab_metadata metadata;
+	var slave_metadata = List<jab_metadata>.filled(4, null); //4
+	Int8 palette;
+	jab_data data;
+}
+
+
+//extern jab_encode* createEncode(jab_int32 color_number, jab_int32 symbol_number);
+//extern void destroyEncode(jab_encode* enc);
+//extern jab_int32 generateJABCode(jab_encode* enc, jab_data* data);
+//extern jab_data* decodeJABCode(jab_bitmap* bitmap, jab_int32 mode, jab_int32* status);
+//extern jab_data* decodeJABCodeEx(jab_bitmap* bitmap, jab_int32 mode, jab_int32* status, jab_decoded_symbol* symbols, jab_int32 max_symbol_number);
+//extern jab_boolean saveImage(jab_bitmap* bitmap, jab_char* filename);
+//extern jab_boolean saveImageCMYK(jab_bitmap* bitmap, jab_boolean isCMYK, jab_char* filename);
+//extern jab_bitmap* readImage(jab_char* filename);
+//extern void reportError(jab_char* message);
+//
+//#endif
