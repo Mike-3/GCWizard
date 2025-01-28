3ff873a809073ac9cec72cc43b77d4c38daccb98
Mike-3 <Mike.Binnemann@web.de>
2025-01-28 16:58:33 +0100

    Squashed commit of the following:
    commit fa574dbedc9a82f52f6b05b83507fb895bd02ef5
    Author: Mark Lorenz <mark@four-two.de>
    Date:   Tue Jan 28 12:38:58 2025 +0100
    
        Reverse enhanced
    
    commit 833f72036180d8d03bc15ac2f1f44f4e23f4cc1c
    Merge: 0c3545240 04974b86a
    Author: Mark Lorenz <mark@four-two.de>
    Date:   Tue Jan 28 12:01:21 2025 +0100
    
        Merge branch '3.4.0' into fix_reverse_projection
    
        # Conflicts:
        #	lib/utils/math_utils.dart
    
    commit 0c354524071982e378acf22b2665ffbb898135ee
    Author: Mark Lorenz <mark@four-two.de>
    Date:   Tue Jan 28 11:58:34 2025 +0100
    
        baselined
    
    commit 04974b86ac3ae3dc4f359f58a274057c43b066a1
    Author: Mike-3 <70719484+Mike-3@users.noreply.github.com>
    Date:   Tue Jan 28 06:29:57 2025 +0100
    
        fix color changes (#2106)
    
        add tests
    
    commit be99f57f5ca6bef7607a019e6414d6eac77630c6
    Author: Mike-3 <Mike.Binnemann@web.de>
    Date:   Mon Jan 27 12:32:41 2025 +0100
    
        fix color changes
    
        add tests
    
    commit 834150c2d5a8c4fa4fc04f634e52c49ced146ff3
    Author: Mike-3 <Mike.Binnemann@web.de>
    Date:   Mon Jan 27 07:13:02 2025 +0100
    
        resize symbol tables
    
    commit c8b6076880ee7692136787692467ef2e9ac99314
    Author: Thomas <thomas@familiezimmermann.de>
    Date:   Mon Jan 27 07:01:06 2025 +0100
    
        Issue 2098 symboltable gestures numbers (#2101)
    
        * symbol table gestures numbers 01
    
        * add symbol table gesture 02
    
        * add symbols
    
    commit 15c49b05c9e9113e2eac48d43f899e48dee893cf
    Author: Mark Lorenz <mark@four-two.de>
    Date:   Fri Jan 24 17:09:57 2025 +0100
    
        combination of karneys approach and our old gives a very good (although not perfect) solution rate and mostly a great performance
    
    commit be8ed5d8def5717cb3009811823db5efe5892702
    Author: Mark Lorenz <mark@four-two.de>
    Date:   Wed Dec 25 17:46:47 2024 +0100
    
        something's fishy
    
    commit 52cd0082433c39903ef5f9e171d07ab30869317f
    Author: Mark Lorenz <mark@four-two.de>
    Date:   Tue Dec 24 12:26:15 2024 +0100
    
        implementing cffk's stackoverflow code, but currently not working.
    
    commit 1640b5aae821dd4e084d7bcbe05d204f9fc012c0
    Author: S-Man42 <info@s-man42.de>
    Date:   Thu Nov 28 23:59:50 2024 +0100
    
        init

--- lib/tools/coords/_common/logic/external_libs/karney.geographic_lib/geographic_lib/geodesic.dart
+++ lib/tools/coords/_common/logic/external_libs/karney.geographic_lib/geographic_lib/geodesic.dart
@@ -312,10 +312,11 @@ class _Geodesic {
    * prolate ellipsoid, an additional condition is necessary for a shortest
    * path: the longitudinal extent must not exceed of 180&deg;.)
    **********************************************************************/
-  GeodesicData direct(double lat1, double lon1, double azi1, double s12) {
-    return _direct(lat1, lon1, azi1, false, s12,
+  GeodesicData direct(double lat1, double lon1, double azi1, bool arcmode, double s12) {
+    return _direct(lat1, lon1, azi1, arcmode, s12,
         _GeodesicMask.LATITUDE | _GeodesicMask.LONGITUDE | _GeodesicMask.AZIMUTH |
-        _GeodesicMask.REDUCEDLENGTH | _GeodesicMask.GEODESICSCALE | _GeodesicMask.AREA
+        _GeodesicMask.REDUCEDLENGTH | _GeodesicMask.GEODESICSCALE | _GeodesicMask.AREA |
+        _GeodesicMask.DISTANCE
     );
   }
 
