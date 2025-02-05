cb82754e2041c558be4f62d77c5cb7ec1283152a
Mike-3 <Mike.Binnemann@web.de>
2025-02-05 11:34:19 +0100

    Squashed commit of the following:
    commit 1ae299640bbc758a7f8d9ae90ce27471533b15da
    Author: Mark Lorenz <mark@four-two.de>
    Date:   Tue Jan 28 13:01:46 2025 +0100
    
        ...
    
    commit 6b694830ec6f02cd77e3619afdec0312bc097644
    Author: Mark Lorenz <mark@four-two.de>
    Date:   Tue Jan 28 12:56:05 2025 +0100
    
        ...
    
    commit 272dc3c4d3db756ea62eb1b69f0719e481047c3e
    Author: Mark Lorenz <mark@four-two.de>
    Date:   Tue Jan 28 12:54:55 2025 +0100
    
        QR Output
    
    commit c2c13a61c4bcea5d1d72092c8fe2b7a1539963ea
    Author: Mark Lorenz <mark@four-two.de>
    Date:   Thu Jan 23 00:07:46 2025 +0100
    
        qr works but i try https://stackoverflow.com/questions/79378258/position-subwidget-always-on-top-of-visible-part-of-parent

--- lib/common_widgets/outputs/gcw_output_text.dart
+++ lib/common_widgets/outputs/gcw_output_text.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:gc_wizard/application/i18n/logic/app_localizations.dart';
 import 'package:gc_wizard/application/theme/theme.dart';
 import 'package:gc_wizard/application/theme/theme_colors.dart';
 import 'package:gc_wizard/common_widgets/buttons/gcw_iconbutton.dart';
 import 'package:gc_wizard/common_widgets/clipboard/gcw_clipboard.dart';
+import 'package:gc_wizard/common_widgets/dialogs/gcw_dialog.dart';
+import 'package:gc_wizard/common_widgets/gcw_text_export.dart';
 import 'package:gc_wizard/common_widgets/gcw_textselectioncontrols.dart';
 
 class GCWOutputText extends StatefulWidget {
@@ -31,27 +34,48 @@ class _GCWOutputTextState extends State<GCWOutputText> {
   @override
   Widget build(BuildContext context) {
     return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Expanded(
-          child: Align(
-              alignment: Alignment.centerLeft,
-              child: SelectableText(
+          child:
+             SelectableText(
                 widget.text ?? '',
                 textAlign: TextAlign.left,
                 style: widget.style ?? (widget.isMonotype ? gcwMonotypeTextStyle() : gcwTextStyle()),
                 selectionControls: GCWTextSelectionControls(),
-              )),
+              )
         ),
         widget.text != null && widget.text!.isNotEmpty && !widget.suppressCopyButton
-            ? GCWIconButton(
-                iconColor: widget.style != null ? widget.style!.color : themeColors().mainFont(),
-                size: IconButtonSize.SMALL,
-                icon: Icons.content_copy,
-                onPressed: () {
-                  var copyText = widget.copyText != null ? widget.copyText.toString() : widget.text ?? '';
-                  insertIntoGCWClipboard(context, copyText);
-                },
-              )
+            ?
+              Column(
+                  children: [
+                    GCWIconButton(
+                      iconColor: widget.style != null ? widget.style!.color : themeColors().mainFont(),
+                      size: IconButtonSize.SMALL,
+                      icon: Icons.content_copy,
+                      onPressed: () {
+                        var copyText = widget.copyText != null ? widget.copyText.toString() : widget.text ?? '';
+                        insertIntoGCWClipboard(context, copyText);
+                      },
+                    ),
+                    GCWIconButton(
+                        size: IconButtonSize.SMALL,
+                        icon: widget.text!.length <= MAX_QR_TEXT_LENGTH_FOR_EXPORT ? Icons.qr_code : Icons.save,
+                        onPressed: () async {
+                          showGCWDialog(
+                              context,
+                              'Output',
+                              GCWTextExport(
+                                  text: widget.text!
+                              ),
+                              [GCWDialogButton(text: i18n(context, 'common_ok'))],
+                              cancelButton: false);
+                        }
+                    )
+                  ],
+                )
+
+
             : Container()
       ],
     );
