5a1ea7c210f9b7aae629728b7c94f939b6e55dc5
Mike-3 <Mike.Binnemann@web.de>
2025-02-19 19:49:05 +0100

    Squashed commit of the following:
    commit 1a9073b48971248133dea5c599b5b98fc26115c0
    Author: Mike-3 <Mike.Binnemann@web.de>
    Date:   Wed Feb 19 19:36:11 2025 +0100
    
        implement signingConfigs
    
    commit 531874faf0152c5e15fb808a0d37447b389d36dc
    Author: Mike-3 <Mike.Binnemann@web.de>
    Date:   Wed Feb 19 11:15:24 2025 +0100
    
        Update pubspec.yaml
    
    commit e35e265a8ef286d1bfa0bf71e94417f55e45439b
    Author: Mike-3 <Mike.Binnemann@web.de>
    Date:   Wed Feb 19 09:43:50 2025 +0100
    
        refactoring android bild

---
+++ android/app/build.gradle.kts
@@ -0,0 +1,59 @@
+import java.io.FileInputStream
+import java.util.Properties
+
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "de.sman42.gc_wizard"
+    compileSdk = 34
+    ndkVersion = flutter.ndkVersion
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_17.toString()
+    }
+
+    defaultConfig {
+        applicationId = "de.sman42.gc_wizard"
+
+        minSdk = 21
+        targetSdk = 34
+        versionCode = flutter.versionCode
+        versionName = flutter.versionName
+    }
+
+    val keystorePropertiesFile = rootProject.file("key.properties")
+    val keystoreProperties = Properties().apply {
+        load(FileInputStream(keystorePropertiesFile))
+    }
+
+    signingConfigs {
+        create("release") {
+            if (keystorePropertiesFile.exists()) {
+                keyAlias = keystoreProperties["keyAlias"]  as String
+                keyPassword = keystoreProperties["keyPassword"] as String
+                storeFile = file(keystoreProperties["storeFile"]  as String)
+                storePassword = keystoreProperties["storePassword"]  as String
+            }
+        }
+    }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs["release"]
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
