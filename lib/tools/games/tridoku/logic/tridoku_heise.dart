05d70e4823fab37be5f13be6c3274619bcf1e434
Mike-3 <Mike.Binnemann@web.de>
2025-04-10 11:35:39 +0200

    Update tridoku_heise.dart

--- lib/tools/games/tridoku/logic/tridoku_heise.dart
+++ lib/tools/games/tridoku/logic/tridoku_heise.dart
@@ -1,5 +1,4 @@
 import 'dart:math';
-import 'dart:typed_data';
 
 class Sudoku {
   List<List<int>> board;
@@ -22,24 +21,24 @@ class Sudoku {
   <Point<int>>[Point<int>(3,1), Point<int>(3,2), Point<int>(3,3), Point<int>(3,4), Point<int>(3,5), Point<int>(4,3), Point<int>(4,4), Point<int>(4,5), Point<int>(5,5)],
   <Point<int>>[Point<int>(6,1), Point<int>(6,2), Point<int>(6,3), Point<int>(6,4), Point<int>(6,5), Point<int>(7,3), Point<int>(7,4), Point<int>(7,5), Point<int>(8,5)],
   <Point<int>>[Point<int>(6,7), Point<int>(6,8), Point<int>(6,9), Point<int>(6,10), Point<int>(6,11), Point<int>(7,9), Point<int>(7,10), Point<int>(7,11), Point<int>(8,11)],
- <Point<int>>[Point<int>(1,0), Point<int>(1,1), Point<int>(1,2), Point<int>(2,1), Point<int>(2,2), Point<int>(2,3)], // Hexagons
-    <Point<int>>[Point<int>(2,0), Point<int>(2,1), Point<int>(2,2), Point<int>(3,1), Point<int>(3,2), Point<int>(3,3)],
-    <Point<int>>[Point<int>(2,2), Point<int>(2,3), Point<int>(2,4), Point<int>(3,3), Point<int>(3,4), Point<int>(3,5)],
- <Point<int>>[Point<int>(3,0), Point<int>(3,1), Point<int>(3,2), Point<int>(4,1), Point<int>(4,2), Point<int>(4,3)],
- <Point<int>>[Point<int>(3,2), Point<int>(3,3), Point<int>(3,4), Point<int>(4,3), Point<int>(4,4), Point<int>(4,5)],
- <Point<int>>[Point<int>(3,4), Point<int>(3,5), Point<int>(3,6), Point<int>(4,5), Point<int>(4,6), Point<int>(4,7)],
- <Point<int>>[Point<int>(5,0), Point<int>(5,1), Point<int>(5,2), Point<int>(6,1), Point<int>(6,2), Point<int>(6,3)],
- <Point<int>>[Point<int>(5,2), Point<int>(5,3), Point<int>(5,4), Point<int>(6,3), Point<int>(6,4), Point<int>(6,5)],
- <Point<int>>[Point<int>(5,4), Point<int>(5,5), Point<int>(5,6), Point<int>(6,5), Point<int>(6,6), Point<int>(6,7)],
- <Point<int>>[Point<int>(5,6), Point<int>(5,7), Point<int>(5,8), Point<int>(6,7), Point<int>(6,8), Point<int>(6,9)],
- <Point<int>>[Point<int>(5,8), Point<int>(5,9), Point<int>(5,10), Point<int>(6,9), Point<int>(6,10), Point<int>(6,11)],
- <Point<int>>[Point<int>(7,0), Point<int>(7,1), Point<int>(7,2), Point<int>(8,1), Point<int>(8,2), Point<int>(8,3)],
- <Point<int>>[Point<int>(7,2), Point<int>(7,3), Point<int>(7,4), Point<int>(8,3), Point<int>(8,4), Point<int>(8,5)],
- <Point<int>>[Point<int>(7,4), Point<int>(7,5), Point<int>(7,6), Point<int>(8,5), Point<int>(8,6), Point<int>(8,7)],
- <Point<int>>[Point<int>(7,6), Point<int>(7,7), Point<int>(7,8), Point<int>(8,7), Point<int>(8,8), Point<int>(8,9)],
- <Point<int>>[Point<int>(7,8), Point<int>(7,9), Point<int>(7,10), Point<int>(8,9), Point<int>(8,10), Point<int>(8,11)],
- <Point<int>>[Point<int>(7,10), Point<int>(7,11), Point<int>(7,12), Point<int>(8,11), Point<int>(8,12), Point<int>(8,13)],
- <Point<int>>[Point<int>(7,12), Point<int>(7,13), Point<int>(7,14), Point<int>(8,13), Point<int>(8,14), Point<int>(8,15)],
+ // <Point<int>>[Point<int>(1,0), Point<int>(1,1), Point<int>(1,2), Point<int>(2,1), Point<int>(2,2), Point<int>(2,3)], // Hexagons
+ //    <Point<int>>[Point<int>(2,0), Point<int>(2,1), Point<int>(2,2), Point<int>(3,1), Point<int>(3,2), Point<int>(3,3)],
+ //    <Point<int>>[Point<int>(2,2), Point<int>(2,3), Point<int>(2,4), Point<int>(3,3), Point<int>(3,4), Point<int>(3,5)],
+ // <Point<int>>[Point<int>(3,0), Point<int>(3,1), Point<int>(3,2), Point<int>(4,1), Point<int>(4,2), Point<int>(4,3)],
+ // <Point<int>>[Point<int>(3,2), Point<int>(3,3), Point<int>(3,4), Point<int>(4,3), Point<int>(4,4), Point<int>(4,5)],
+ // <Point<int>>[Point<int>(3,4), Point<int>(3,5), Point<int>(3,6), Point<int>(4,5), Point<int>(4,6), Point<int>(4,7)],
+ // <Point<int>>[Point<int>(5,0), Point<int>(5,1), Point<int>(5,2), Point<int>(6,1), Point<int>(6,2), Point<int>(6,3)],
+ // <Point<int>>[Point<int>(5,2), Point<int>(5,3), Point<int>(5,4), Point<int>(6,3), Point<int>(6,4), Point<int>(6,5)],
+ // <Point<int>>[Point<int>(5,4), Point<int>(5,5), Point<int>(5,6), Point<int>(6,5), Point<int>(6,6), Point<int>(6,7)],
+ // <Point<int>>[Point<int>(5,6), Point<int>(5,7), Point<int>(5,8), Point<int>(6,7), Point<int>(6,8), Point<int>(6,9)],
+ // <Point<int>>[Point<int>(5,8), Point<int>(5,9), Point<int>(5,10), Point<int>(6,9), Point<int>(6,10), Point<int>(6,11)],
+ // <Point<int>>[Point<int>(7,0), Point<int>(7,1), Point<int>(7,2), Point<int>(8,1), Point<int>(8,2), Point<int>(8,3)],
+ // <Point<int>>[Point<int>(7,2), Point<int>(7,3), Point<int>(7,4), Point<int>(8,3), Point<int>(8,4), Point<int>(8,5)],
+ // <Point<int>>[Point<int>(7,4), Point<int>(7,5), Point<int>(7,6), Point<int>(8,5), Point<int>(8,6), Point<int>(8,7)],
+ // <Point<int>>[Point<int>(7,6), Point<int>(7,7), Point<int>(7,8), Point<int>(8,7), Point<int>(8,8), Point<int>(8,9)],
+ // <Point<int>>[Point<int>(7,8), Point<int>(7,9), Point<int>(7,10), Point<int>(8,9), Point<int>(8,10), Point<int>(8,11)],
+ // <Point<int>>[Point<int>(7,10), Point<int>(7,11), Point<int>(7,12), Point<int>(8,11), Point<int>(8,12), Point<int>(8,13)],
+ // <Point<int>>[Point<int>(7,12), Point<int>(7,13), Point<int>(7,14), Point<int>(8,13), Point<int>(8,14), Point<int>(8,15)],
     ];
 
   void printBoard() {
@@ -50,6 +49,46 @@ class Sudoku {
 
   int _columnCount(int row) => (row * 2) + 1;
 
+  List<List<Point<int>>> hexagons(int row, int column) {
+    var lists = <List<Point<int>>>[];
+    var list = <Point<int>>[];
+
+    void addCell(int r, int c) {
+      if (r > 0 && r < 9 && c > 0 && c < _columnCount(r)) list.add(Point<int>(r,c));
+    }
+    void addCells(List<Point<int>> list ) {
+      if (list.isNotEmpty)  lists.add(list);
+    }
+
+    for (int c = column - 3; c < column - 1; c++) {
+      addCell(row - 1, c);
+    }
+    for (int c = column - 2; c <= column; c++) {
+      addCell(row , c);
+    }
+    addCells(list);
+
+    list = <Point<int>>[];
+    for (int c = column - 1; c < column + 1; c++) {
+      addCell(row - 1, c);
+    }
+    for (int c = column; c <= column + 2; c++) {
+      addCell(row , c);
+    }
+    addCells(list);
+
+    list = <Point<int>>[];
+    for (int c = column - 1; c < column + 1; c++) {
+      addCell(row, c);
+    }
+    for (int c = column; c <= column + 2; c++) {
+      addCell(row , c);
+    }
+    addCells(list);
+
+    return lists;
+  }
+
   bool numberIsValid(int row, int column, int number) {
     // _unitlist.where((pList) => pList.contains(Point<int>(row, column))).forEach((pList) {
     //   if (pList.any((point) => board[point.y][point.x]  == number)) return false;
@@ -57,16 +96,19 @@ class Sudoku {
     // printBoard();
     var lists = _unitlist.where((pList) => pList.contains(Point<int>(row, column)));
     for (var pList in lists) {
-      // print(pList.toString());
-      // if (pList.any((point) => board[point.y][point.x] == number)) return false;
-      if (pList.any((point) {
-        // print(point.toString() + ' ' + board[point.x].length.toString());
-        return board[point.x][point.y] == number;
-      })) {
-        return false;
-      }
+       // print(pList.toString());
+      if (pList.any((point) => board[point.x][point.y] == number)) return false;
+      // if (pList.any((point) {
+      //   // print(point.toString() + ' ' + board[point.x].length.toString());
+      //   return board[point.x][point.y] == number;
+      // })) {
+      //   return false;
+      // }
     }
-
+    lists = hexagons(row, column);
+    for (var pList in lists) {
+      if (pList.any((point) => board[point.x][point.y] == number)) return false;
+    }
     return true;
   }
 
@@ -91,7 +133,7 @@ class Sudoku {
 }
 
 Future<void> main() async {
-  List<List<int>> board = [
+  List<List<int>> board = [ //https://logic-masters.de/Raetselportal/Raetsel/zeigen.php?chlang=en&id=000DEM
     [6],
     [0, 0, 4],
     [0, 2, 0, 8, 0],
