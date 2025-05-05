4ff39689a6c4685bb790c2b2e57eeb1ee2f15b68
Mike-3 <Mike.Binnemann@web.de>
2025-05-05 19:40:56 +0200

    Update all_tools_view.dart

--- lib/application/category_views/all_tools_view.dart
+++ lib/application/category_views/all_tools_view.dart
@@ -329,6 +329,7 @@ class _MainViewState extends State<MainView> {
   var _isSearching = false;
   late TextEditingController _searchController;
   final _scaffoldKey = GlobalKey<ScaffoldState>();
+  final _searchKey = GlobalKey<_MainViewState>();
   var _searchText = '';
   final _SHOW_SUPPORT_HINT_EVERY_N = 50;
 
@@ -424,7 +425,6 @@ class _MainViewState extends State<MainView> {
     Favorites.initialize();
 
     var toolList = (_isSearching && _searchText.isNotEmpty) ? _getSearchedList() : null;
-
     if (!(_isSearching && _searchText.isNotEmpty)) {
       return DefaultTabController(
         length: 3,
@@ -493,6 +493,7 @@ class _MainViewState extends State<MainView> {
   Widget _buildTitleAndSearchTextField() {
     return _isSearching
         ? GCWTextField(
+            key: _searchKey,
             controller: _searchController,
             autofocus: true,
             icon: Icon(Icons.search, color: themeColors().mainFont()),
@@ -517,7 +518,6 @@ class _MainViewState extends State<MainView> {
 
   List<GCWTool> _getSearchedList() {
     var _sanitizedSearchText = removeAccents(_searchText.toLowerCase()).replaceAll(NOT_ALLOWED_SEARCH_CHARACTERS, '');
-
     if (_sanitizedSearchText.isEmpty) return <GCWTool>[];
 
     Set<String> _queryTexts = _sanitizedSearchText.split(REGEXP_SPLIT_STRINGLIST).toSet();
