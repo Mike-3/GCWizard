3e13adc94d34b2ab67d4b1289eb12d8c37b8cc14
Mike-3 <70719484+Mike-3@users.noreply.github.com>
2025-09-01 19:09:00 +0200

    Update hsv_picker.dart (#2348)

--- lib/common_widgets/color_pickers/external_libs/fluttercandies.flutter_hsvcolor_picker/hsv_picker.dart
+++ lib/common_widgets/color_pickers/external_libs/fluttercandies.flutter_hsvcolor_picker/hsv_picker.dart
@@ -61,7 +61,7 @@ class _SliderPickerState extends State<_SliderPicker> {
           LayoutId(
               id: _SliderLayout.thumb,
               child: Transform(
-                transform: Matrix4.identity()..translateByDouble(_ThumbPainter.getWidth(getRatio(), maxWidth), 0, 0, 0),
+                transform: Matrix4.identity()..translate(_ThumbPainter.getWidth(getRatio(), maxWidth)),
                 child: CustomPaint(painter: _ThumbPainter()),
               )),
 
