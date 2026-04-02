fb933063520411a80cf24ec52752d1644869966c
Thomas <thomas@familiezimmermann.de>
2026-04-02 19:32:51 +0200

    change order of coord selection. triangle selection is the last entry (#2456)

--- lib/application/category_views/selector_lists/coords_selection.dart
+++ lib/application/category_views/selector_lists/coords_selection.dart
@@ -10,7 +10,10 @@ class CoordsSelection extends GCWSelection {
   @override
   Widget build(BuildContext context) {
     final List<GCWTool> _toolList =
-        registeredTools.where((element) => element.categories.contains(ToolCategory.COORDINATES)).toList();
+        registeredTools.where((element) => element.categories.contains(ToolCategory.COORDINATES)
+        && element.id != 'triangle_ellipsoid_selection').toList();
+
+    _toolList.add(registeredTools.firstWhere((element) => element.id == 'triangle_ellipsoid_selection'));
 
     return GCWToolList(toolList: _toolList);
   }
